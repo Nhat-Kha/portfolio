ddb67781f1fcae649781a6da9050a7a30acd27e4
Kaio-Nguyen <nhatkha06299@gmail.com>
2024-03-29 12:41:09 +0700

    add all page

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -2,13 +2,19 @@
 
 import React from "react";
 import { motion } from "framer-motion";
+import SectionHeading from "./section-heading";
 // import { useSectionInView } from "@/lib/hooks";
 
 export default function About() {
   //   const ref = useSectionInView("about");
   return (
-    <motion.section className="mb-28 max-w-[45rem] text-center leading-8">
-      <h2 className="text-3xl font-medium capitalize mb-8">About me</h2>
+    <motion.section
+      className="mb-28 max-w-[45rem] text-center leading-8"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+    >
+      <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
         After graduating with a degree in{" "}
         <span className="font-medium">Accounting</span>, I decided to pursue my
